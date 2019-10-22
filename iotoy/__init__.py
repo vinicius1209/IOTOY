5f492f66b9eec707414561b3743445c594e5157f
Vinicius Machado <viniciusadrianomachado@gmail.com>
2019-10-22 11:56:59 -0300

    Ajustado erro do config.py no init

--- iotoy/__init__.py
+++ iotoy/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask, session
 from flask_cors import CORS
-from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
 
 # Custum Class 
 class FlaskVue(Flask):
