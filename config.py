1e6b9d5d360827e75efa0e1aeb45255c54597734
Vinicius Machado <viniciusadrianomachado@gmail.com>
2019-10-22 13:26:57 -0300

    Ajustado basedir no Config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from os.path import join, dirname, realpath
-basedir = os.path.abspath(os.path.dirname(__file__)) + '/app'
+basedir = os.path.abspath(os.path.dirname(__file__)) + '/iotoy'
 class Config(object):
 	SECRET_KEY = 'iotoyComputacao'
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
