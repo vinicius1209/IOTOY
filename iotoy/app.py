1ac58f07cca5dcb3502a07fc7d9c1bd31947d417
Vinicius Machado <viniciusadrianomachado@gmail.com>
2019-10-22 12:14:18 -0300

    adadasdasd

--- iotoy/app.py
+++ iotoy/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 # Custum Class 
 class FlaskVue(Flask):
