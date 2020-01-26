2c0982fd5f5e2983e153aeda44957d51b59e73f5
Vinícius Adriano Machado <viniciusadrianomachado@gmail.com>
2020-01-26 20:07:51 -0300

    Add files via upload

--- iotoy/templates/router/config.vue.js
+++ iotoy/templates/router/config.vue.js
@@ -3,7 +3,16 @@ const Config = {
     <v-container fluid grid-list-md>
         <v-layout row wrap>
             <v-flex lg12 md12 xs12>
-                <v-card class="elevation-3">
+                <v-toolbar flat color="white">
+                    <v-toolbar-title>Parâmetros do IOTOY</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items>
+                        <v-btn flat color="success" @click="set_user_config">Salvar</v-btn>
+                    </v-toolbar-items>
+                </v-toolbar>
+            </v-flex>
+            <v-flex lg6 md12 xs12>
+                <v-card flat>
                     <v-card-title primary-title>
                     <div>
                         <h3 class="headline mb-0">Plano Lite</h3>
@@ -17,16 +26,44 @@ const Config = {
                         </v-form>
                     </v-card-text>
                     <v-card-actions>
-                        <v-btn color="success" @click="set_user_config">Salvar</v-btn>
-                        <v-btn color="info" href="https://cloud.ibm.com" target="_blank"> IBM Cloud </v-btn>
+                        <v-btn flat color="info" href="https://cloud.ibm.com" target="_blank"> IBM Cloud </v-btn>
                     </v-card-actions>
                 </v-card>
             </v-flex>
+            <v-flex lg6 md12 xs12>
+                <v-card flat>
+                    <v-card-title primary-title>
+                    <div>
+                        <h3 class="headline mb-0">Geração das falas</h3>
+                    </div>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-switch v-model="robotic_sound" label="Habilitar voz robótica"></v-switch>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+
+            <v-dialog v-model="dialog_robotc" persistent max-width="290">
+                <v-card>
+                    <v-card-title class="headline">Aviso</v-card-title>
+                    <v-card-text>
+                        O parâmetro "Voz robótica" está habilitado.
+                        Portanto o tempo de resposta da geração de falas
+                        será maior do que o normal.
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="green darken-1" flat @click="dialog_robotc = false">Entendi</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+
         </v-layout>
     </v-container>
      `,
     data() {
         return {
+            dialog_robotc: false,
             valid: true,
             api_key: null,
             api_keyRules: [
@@ -35,11 +72,20 @@ const Config = {
             api_url: null,
             api_urlRules: [
                 v => !!v || 'É necessário preencher a URL'
-            ]
+            ],
+            robotic_sound: false
         }
     },
     mounted() {
         this.get_user_config()
+    },
+    watch:{
+        robotic_sound: function (val) {
+            this.robotic_sound = val
+            if (val == true){
+                this.dialog_robotc = true
+            }
+        }
     },
     methods: {
         get_user_config() {
@@ -48,7 +94,8 @@ const Config = {
                 .then(
                     response => (
                         this.api_key = response.data.api_key,
-                        this.api_url = response.data.api_url
+                        this.api_url = response.data.api_url,
+                        this.robotic_sound = response.data.robotic_sound
                     )
                 )
                 .catch(error => {
@@ -63,7 +110,8 @@ const Config = {
                         "/current_user/config",
                         {
                             api_key: this.api_key,
-                            api_url: this.api_url
+                            api_url: this.api_url,
+                            robotic_sound: this.robotic_sound
                         },
                         {
                             headers: {
